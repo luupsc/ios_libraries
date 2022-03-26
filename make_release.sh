50b0ed7a12b330da83423cbe71673bb4af9f1689
tarunon <croissant9603@gmail.com>
2022-03-26 14:57:46 +0900

    modify: remove unused libs

--- make_release.sh
+++ make_release.sh
@@ -4,9 +4,7 @@ if [ ! -d "Release" ]; then
 fi
 
 cd Carthage/Build
-zip -r -X ../../Release/CHIPageControl.xcframework.zip CHIPageControl.xcframework
 zip -r -X ../../Release/SVProgressHUD.xcframework.zip SVProgressHUD.xcframework
-zip -r -X ../../Release/SlideMenuControllerSwift.xcframework.zip SlideMenuControllerSwift.xcframework
 cd ../../Build
 zip -r -X ../Release/GoogleMaps.xcframework.zip GoogleMaps.xcframework
 zip -r -X ../Release/GoogleMapsBase.xcframework.zip GoogleMapsBase.xcframework
@@ -15,9 +13,7 @@ zip -r -X ../Release/GoogleMapsM4B.xcframework.zip GoogleMapsM4B.xcframework
 zip -r -X ../Release/GooglePlaces.xcframework.zip GooglePlaces.xcframework
 
 cd ../Release
-shasum -a 256 "CHIPageControl.xcframework.zip" >> checksum
 shasum -a 256 "SVProgressHUD.xcframework.zip" >> checksum
-shasum -a 256 "SlideMenuControllerSwift.xcframework.zip" >> checksum
 shasum -a 256 "GoogleMaps.xcframework.zip" >> checksum
 shasum -a 256 "GoogleMapsBase.xcframework.zip" >> checksum
 shasum -a 256 "GoogleMapsCore.xcframework.zip" >> checksum
