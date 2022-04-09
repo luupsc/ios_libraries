dc615eeb756e03466b3f551c8f0bd974cfa096fc
tarunon <croissant9603@gmail.com>
2022-04-09 14:33:30 +0900

    modify: use googlemaps xcframeworks support beta

--- Package.swift
+++ Package.swift
@@ -26,28 +26,28 @@ let package = Package(
   targets: [
     .binaryTarget(
         name: "SVProgressHUD",
-        url: "https://github.com/luupsc/ios_libraries/releases/download/0.3.7/SVProgressHUD.xcframework.zip",
-        checksum: "c7539a5c363a0d8a84515237de29d04291520693825357e6b9cf8e279043e551"
+        url: "https://github.com/luupsc/ios_libraries/releases/download/0.4.0/SVProgressHUD.xcframework.zip",
+        checksum: "e369f8f763f601e0bd6425b49f074df270d50cba625193b150da56859cc68269"
     ),
     .binaryTarget(
         name: "GoogleMaps",
-        url: "https://github.com/luupsc/ios_libraries/releases/download/0.3.7/GoogleMaps.xcframework.zip",
-        checksum: "f84983d52ffe00326424cf0f3ccfc45b02553e81f6c02079ce3a885cf9245a02"
+        url: "https://github.com/luupsc/ios_libraries/releases/download/0.4.0/GoogleMaps.xcframework.zip",
+        checksum: "e61c0106f98920a9e6080e6b9466c54dd720a4d2bb7e6b65addd5a2d0c28246f"
     ),
     .binaryTarget(
         name: "GoogleMapsBase",
-        url: "https://github.com/luupsc/ios_libraries/releases/download/0.3.7/GoogleMapsBase.xcframework.zip",
-        checksum: "956dc85a2454294e8884e82b61fbd523950f01f98d008dbe283aa3743de96017"
+        url: "https://github.com/luupsc/ios_libraries/releases/download/0.4.0/GoogleMapsBase.xcframework.zip",
+        checksum: "9f0c29659310f06b593848b212a80c378dc47d4719557ad457a9f453b3054519"
     ),
     .binaryTarget(
         name: "GoogleMapsCore",
-        url: "https://github.com/luupsc/ios_libraries/releases/download/0.3.7/GoogleMapsCore.xcframework.zip",
-        checksum: "3d6dfa3fa43a13480272b61e854432906f9953e689d6d6a4a37ca9f99fefc6ab"
+        url: "https://github.com/luupsc/ios_libraries/releases/download/0.4.0/GoogleMapsCore.xcframework.zip",
+        checksum: "ee91db5c4ce1c504e15650419a987992b60d7b5096bff3551883953ae143f8ab"
     ),
     .binaryTarget(
         name: "GooglePlaces",
-        url: "https://github.com/luupsc/ios_libraries/releases/download/0.3.7/GooglePlaces.xcframework.zip",
-        checksum: "40e6cb6829f36d3829dadbc949959074bdcc07d54e8fe3fbf2840b5032709564"
+        url: "https://github.com/luupsc/ios_libraries/releases/download/0.4.0/GooglePlaces.xcframework.zip",
+        checksum: "88c57771e0a0a20dcf8b09d053e97b4e127ddbb02fa149e28378269dc0a8b82d"
     )
   ]
 )
--- make_release.sh
+++ make_release.sh
@@ -5,14 +5,13 @@ fi
 
 cd Carthage/Build
 zip -r -X ../../Release/SVProgressHUD.xcframework.zip SVProgressHUD.xcframework
-cd ../../Build
-zip -r -X ../Release/GoogleMaps.xcframework.zip GoogleMaps.xcframework
-zip -r -X ../Release/GoogleMapsBase.xcframework.zip GoogleMapsBase.xcframework
-zip -r -X ../Release/GoogleMapsCore.xcframework.zip GoogleMapsCore.xcframework
-zip -r -X ../Release/GoogleMapsM4B.xcframework.zip GoogleMapsM4B.xcframework
-zip -r -X ../Release/GooglePlaces.xcframework.zip GooglePlaces.xcframework
+zip -r -X ../../Release/GoogleMaps.xcframework.zip GoogleMaps.xcframework
+zip -r -X ../../Release/GoogleMapsBase.xcframework.zip GoogleMapsBase.xcframework
+zip -r -X ../../Release/GoogleMapsCore.xcframework.zip GoogleMapsCore.xcframework
+zip -r -X ../../Release/GoogleMapsM4B.xcframework.zip GoogleMapsM4B.xcframework
+zip -r -X ../../Release/GooglePlaces.xcframework.zip GooglePlaces.xcframework
 
-cd ../Release
+cd ../../Release
 shasum -a 256 "SVProgressHUD.xcframework.zip" >> checksum
 shasum -a 256 "GoogleMaps.xcframework.zip" >> checksum
 shasum -a 256 "GoogleMapsBase.xcframework.zip" >> checksum
